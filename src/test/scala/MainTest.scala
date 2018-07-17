1bd48988368d3c3db4453448e97cb5a407f2f7d7
fhsiao <frank.hsiao@hotmail.com>
2018-07-16 22:05:02 -0600

    Scala-Boot: Fix .gitignore to versioning the complete project

---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,17 @@
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+class MainTest extends FunSuite with BeforeAndAfterEach {
+
+  override def beforeEach() {
+
+  }
+
+  override def afterEach() {
+
+  }
+
+  test("testMain") {
+
+  }
+
+}
